68b1f70d192c4a5fd2de83df2c19db7e573bf694
Romain Janvier <romain.janvier@hotmail.fr>
2024-02-12 20:48:02 +0100

    Added iostream capture

--- wrapper/dendroptimized_cpy.cpp
+++ wrapper/dendroptimized_cpy.cpp
@@ -1,4 +1,5 @@
 
+#include <pybind11/iostream.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -8,10 +9,14 @@
 namespace py = pybind11;
 using namespace pybind11::literals;
 
-
 PYBIND11_MODULE(dendroptimized, m)
 {
     m.doc() = "Dendroptimized module";
-    m.def("voxelate", &dendroptimized::voxelate_ll<double>, "xyz"_a.noconvert(), "res_xy"_a, "res_z"_a, "n_digits"_a, "id_x"_a=0, "id_y"_a=1, "id_z"_a=2);
-    m.def("voxelate_legacy", &dendroptimized::voxelate<double>, "xyz"_a.noconvert(), "res_xy"_a, "res_z"_a, "n_digits"_a, "id_x"_a=0, "id_y"_a=1, "id_z"_a=2);
+    m.def(
+        "voxelate", &dendroptimized::voxelate_ll<double>, "xyz"_a.noconvert(), "res_xy"_a, "res_z"_a, "n_digits"_a,
+        "id_x"_a = 0, "id_y"_a = 1, "id_z"_a = 2,
+        py::call_guard<py::scoped_ostream_redirect, py::scoped_estream_redirect>());
+    m.def(
+        "voxelate_legacy", &dendroptimized::voxelate<double>, "xyz"_a.noconvert(), "res_xy"_a, "res_z"_a, "n_digits"_a,
+        "id_x"_a = 0, "id_y"_a = 1, "id_z"_a = 2);
 }
