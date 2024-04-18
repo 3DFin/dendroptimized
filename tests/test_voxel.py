9dcc5ce656a73d2dd17e1eea9247bbb66245cb7a
Romain Janvier <romain.janvier@hotmail.fr>
2024-04-18 21:37:56 +0200

    Add a test

--- tests/test_voxel.py
+++ tests/test_voxel.py
@@ -1,4 +1,17 @@
+import numpy as np
 from dendromatics import voxel
+from dendroptimized import voxelize
+
 
 def test_voxel():
-    return true
+    xyz = np.random.rand(10_000_000, 3) * 100
+    cloud_orig, vox2c_orig, c2vox_orig = voxel.voxelate(
+        xyz, 0.3, 0.3, 5, with_n_points=False
+    )
+    cloud_opti, vox2c_opti, c2vox_opti = voxelize(xyz, 0.3, 0.3, 5)
+    np.testing.assert_allclose(cloud_opti, cloud_orig)
+    np.testing.assert_equal(vox2c_orig, vox2c_opti)
+
+    # This test below won't work as neither dendromatics
+    # nor dendroptimized use a stable sort
+    # np.testing.assert_equal(c2vox_orig, c2vox_opti)
