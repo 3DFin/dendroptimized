33242a3bc4d7de22e2aeb1e9bca12c49b6186ac4
Romain Janvier <romain.janvier@hotmail.fr>
2024-04-19 15:36:32 +0200

    Add benchmarks

--- tests/test_voxel.py
+++ tests/test_voxel.py
@@ -13,3 +13,22 @@ def test_voxel():
     # This test below won't work as neither dendromatics
     # nor dendroptimized use a stable sort
     # np.testing.assert_equal(c2vox_orig, c2vox_opti)
+
+
+def fixture():
+    np.random.seed(1)
+    return np.random.uniform(0, 100, size=(10_000_000, 3))
+
+
+def test_bench_dendromatics(benchmark):
+    def _to_bench():
+        voxel.voxelate(fixture(), 0.3, 0.3, 5, with_n_points=False)
+
+    benchmark(_to_bench)
+
+
+def test_bench_dendroptimized(benchmark):
+    def _to_bench():
+        voxelize(fixture(), 0.3, 0.3, 5)
+
+    benchmark(_to_bench)
